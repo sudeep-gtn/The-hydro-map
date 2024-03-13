5100e3df213192c35a97db2c4669ec8e94f9f051
sudeep-gtn <sudeep.bogati@greentick.com.np>
2024-03-13 17:31:51 +0545

    OSM Map

--- app/hydropowers/page.tsx
+++ app/hydropowers/page.tsx
@@ -1,13 +1,14 @@
 import dynamic from 'next/dynamic'
-// import OpenStreetMap from '../component/OpenStreetMap'
-const Map2 = dynamic(() => import('../../components/Map'), {
+import Nav from '../../components/Nav'
+// import MyMapComponent from '../../components/Map'
+const Map = dynamic(() => import('../../components/Map'), {
   ssr: false,
 })
 const index = () => {
   return (
     <>
-      <h1 className='text-center'>OpenStreetMap</h1>
-      <Map2 />
+      <Nav />
+      <Map />
     </>
   )
 }
--- components/Map.tsx
+++ components/Map.tsx
@@ -1,58 +1,92 @@
-'use client';
-import React, { useState, useRef, RefObject } from 'react'
-import { Map, LeafletMouseEvent, LatLngTuple } from 'leaflet';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
-import 'leaflet/dist/leaflet.css'
+"use client";
+import React, { useState, useRef, RefObject } from "react";
+import { Map, LeafletMouseEvent, LatLngTuple, map } from "leaflet";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { useMap, useMapEvent } from "react-leaflet/hooks";
+import "leaflet/dist/leaflet.css";
+import "tailwindcss/tailwind.css";
+import '../styles/map.css';
 
-const Map2 = () => {
-    const [center, setCenter] = useState({ lat: -4.043477, lng: 39.668205 })
-    const ZOOM_LEVEL = 9
-    const mapRef = useRef<Map>(null);
-    const [location, setLocation] = useState({ loaded: false, error: null, coordinates: { lat: 27.696277881963407, lng: 85.36042867577738 } });
 
-    return (
-        <>
 
-            {/* -------------------------------------------------------------------------- */}
-            {/* <MapContainer center={[location.coordinates.lat, location.coordinates.lng]} zoom={13} scrollWheelZoom={false}>
-                <TileLayer
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
-                <Marker position={[location.coordinates.lat, location.coordinates.lng]}>
-                    <Popup>
-                        A pretty CSS3 popup. <br /> Easily customizable.
-                    </Popup>
-                </Marker>
-            </MapContainer> */}
-            {/* -------------------------------------------------------------------------- */}
-            <div className='container'>
-                <div className='container'>
-                    <h1 className='text-center-mt-5'>OpenStreetMap Embeded</h1>
-                </div>
-                <div className='row'>
-                    Lorem, ipsum dolor.
-                    <div className='col'>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo optio suscipit velit repudiandae, dolore aspernatur. Facere laudantium ea, sunt sit impedit saepe illum id quod quo doloribus cum quaerat quidem.
-                        <div className='container'>
-                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque aliquam cum dicta, quae totam cupiditate! Quis voluptatum tenetur alias voluptate accusamus debitis, minus voluptas animi? Reprehenderit vel quae porro impedit.
-                            <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-                                <TileLayer
-                                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                                />
-                                <Marker position={[location.coordinates.lat,location.coordinates.lng]}>
-                                    <Popup>
-                                        A pretty CSS3 popup. <br /> Easily customizable.
-                                    </Popup>
-                                </Marker>
-                            </MapContainer>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </>
-    )
+
+// const Map2 = () => {
+//     const map = useMap();
+//     const [center, setCenter] = useState({ lat: -4.043477, lng: 39.668205 })
+//     const ZOOM_LEVEL = 9
+//     const mapRef = useRef<Map>(null);
+//     const [location, setLocation] = useState({ loaded: false, error: null, coordinates: { lat: 27.696277881963407, lng: 85.36042867577738 } });
+
+//     return (
+//         <>
+
+//             {/* -------------------------------------------------------------------------- */}
+//             {/* <MapContainer center={[location.coordinates.lat, location.coordinates.lng]} zoom={13} scrollWheelZoom={false}>
+//                 <TileLayer
+//                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+//                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+//                 />
+//                 <Marker position={[location.coordinates.lat, location.coordinates.lng]}>
+//                     <Popup>
+//                         A pretty CSS3 popup. <br /> Easily customizable.
+//                     </Popup>
+//                 </Marker>
+//             </MapContainer> */}
+//             {/* -------------------------------------------------------------------------- */}
+//             <div className='container'>
+//                 <div className='container'>
+//                     <h1 className='text-center-mt-5'>OpenStreetMap Embeded</h1>
+//                 </div>
+//                 <div className='row'>
+//                     <div className='col'>
+//                         <div className='container'>
+//                             <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+//                                 <TileLayer
+//                                     // attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+//                                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+//                                 />
+// <Marker position={[location.coordinates.lat,location.coordinates.lng]}>
+//     <Popup>
+//         A pretty CSS3 popup. <br/> Easily customizable.
+//     </Popup>
+// </Marker>
+//                             </MapContainer>
+//                         </div>
+//                     </div>
+//                 </div>
+//             </div>
+//         </>
+//     )
+// }
+
+// export default Map2
+
+// =======================================================================================================================================
+// function MyComponent() {
+//     const map = useMapEvent('click', () => {
+//         map.setView([51.505, -0.09])
+//     })
+// }
+function MyMapComponent() {
+  return (
+    <div className="map-container"> {/* Apply custom styles to this container */}
+      <MapContainer
+        center={[27.708572067184317, 85.33439213989928]}
+        zoom={12}
+        scrollWheelZoom={false}
+        style={{ height: "100%" }} // Set height and width inline
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[27.708572067184317, 85.33439213989928]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </div>
+  );
 }
-
-export default Map2
+export default MyMapComponent;
