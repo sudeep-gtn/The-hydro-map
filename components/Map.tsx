a3ea84c56cda483f1eb6d3d44f49ba10b4e471c4
sudeep-gtn <sudeep.bogati@greentick.com.np>
2024-03-14 17:22:00 +0545

    Tile layers using GeoJSON file

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,90 +1,85 @@
 "use client";
-import React, { useState, useRef, RefObject } from "react";
+import React, { useState, useRef, RefObject, useEffect } from "react";
+
 import { Map, LeafletMouseEvent, LatLngTuple, map } from "leaflet";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import { useMap, useMapEvent } from "react-leaflet/hooks";
-import "leaflet/dist/leaflet.css";
+import { MapContainer, TileLayer, Marker, Popup, Rectangle, Polyline, GeoJSON, useMap } from "react-leaflet";
+import VectorTileLayer from "react-leaflet-vector-tile-layer";
+
 import "tailwindcss/tailwind.css";
 import '../styles/map.css';
 
+import LocationMarker from "./maps/LocationMarker";
+
+//GeoJSON files 
+import districtTiles from '../data/map-tiles/nepal-districts-new.json';
+import provinceTiles from '../data/map-tiles/states.json';
+
+const myStyles = { color: 'black', fillColor: 'none', weight: 1 };
+function DistrictGeo({ visible, data }: any) {
+  return visible ? <GeoJSON data={data} style={myStyles} /> : null;
+}
+function ProvinceGeoJSON({ visible, data }: any) {
+  return visible ? <GeoJSON data={data} style={myStyles} /> : null;
+}
 
 
-
-// const Map2 = () => {
-//     const map = useMap();
-//     const [center, setCenter] = useState({ lat: -4.043477, lng: 39.668205 })
-//     const ZOOM_LEVEL = 9
-//     const mapRef = useRef<Map>(null);
-//     const [location, setLocation] = useState({ loaded: false, error: null, coordinates: { lat: 27.696277881963407, lng: 85.36042867577738 } });
-
-//     return (
-//         <>
-
-//             {/* -------------------------------------------------------------------------- */}
-//             {/* <MapContainer center={[location.coordinates.lat, location.coordinates.lng]} zoom={13} scrollWheelZoom={false}>
-//                 <TileLayer
-//                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-//                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-//                 />
-//                 <Marker position={[location.coordinates.lat, location.coordinates.lng]}>
-//                     <Popup>
-//                         A pretty CSS3 popup. <br /> Easily customizable.
-//                     </Popup>
-//                 </Marker>
-//             </MapContainer> */}
-//             {/* -------------------------------------------------------------------------- */}
-//             <div className='container'>
-//                 <div className='container'>
-//                     <h1 className='text-center-mt-5'>OpenStreetMap Embeded</h1>
-//                 </div>
-//                 <div className='row'>
-//                     <div className='col'>
-//                         <div className='container'>
-//                             <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-//                                 <TileLayer
-//                                     // attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-//                                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-//                                 />
-// <Marker position={[location.coordinates.lat,location.coordinates.lng]}>
-//     <Popup>
-//         A pretty CSS3 popup. <br/> Easily customizable.
-//     </Popup>
-// </Marker>
-//                             </MapContainer>
-//                         </div>
-//                     </div>
-//                 </div>
-//             </div>
-//         </>
-//     )
-// }
-
-// export default Map2
-
-// =======================================================================================================================================
-// function MyComponent() {
-//     const map = useMapEvent('click', () => {
-//         map.setView([51.505, -0.09])
-//     })
-// }
 function MyMapComponent() {
+  const rectangle: LatLngTuple[] = [
+    [30.5056, 80.058],
+    [26.3475, 88.3015] 
+  ];
+
+  const [zoomLevel, setZoomLevel] = useState(7.5);
+  // --------------------------------------------------------------------------------------------
+  function MapContent() {
+    const map = useMap();
+
+    useEffect(() => {
+      const updateZoomLevel = () => {
+        setZoomLevel(Math.round(map.getZoom()));
+      };
+
+      map.on('zoomend', updateZoomLevel);
+
+      return () => {
+        map.off('zoomend', updateZoomLevel);
+      };
+    }, [map]);
+
+    return (
+      <>
+        <TileLayer
+          minZoom={7}
+          attribution='sudeep'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+
+        {/* Render either district or province GeoJSON based on zoom level */}
+        {zoomLevel > 8.5 ? <DistrictGeo visible={true} data={districtTiles} /> : <ProvinceGeoJSON visible={true} data={provinceTiles} />}
+
+        <LocationMarker />
+
+        <Marker position={[27.708572067184317, 85.33439213989928]}>
+          <Popup>
+            <h1> Greentick Nepal</h1> Contact us for IS edit.
+          </Popup>
+        </Marker>
+      </>
+    );
+  }
+
   return (
     <div className="map-container"> {/* Apply custom styles to this container */}
       <MapContainer
-        center={[27.708572067184317, 85.33439213989928]}
-        zoom={12}
-        scrollWheelZoom={false}
+        center={[28.054508481737447, 84.1650599076575]}
+        zoom={zoomLevel}
+        scrollWheelZoom={true}
         style={{ height: "100%" }} // Set height and width inline
       >
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={[27.708572067184317, 85.33439213989928]}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+
+
+        <MapContent />
+        <Rectangle opacity={0.8} bounds={rectangle} pathOptions={{fillColor: 'black', fillOpacity: 0.05,color:'black' , weight : 1 }} />
       </MapContainer>
     </div>
   );
---
+++ components/maps/LocationMarker.tsx
@@ -0,0 +1,29 @@
+import React ,{useState} from "react"
+import { Marker,useMapEvents, Popup } from "react-leaflet"
+
+interface Position {
+    lat: number;
+    lng: number;
+};
+function LocationMarker() {
+
+    const [position, setPosition] = useState<Position | null>(null);
+    const map = useMapEvents({
+      click() {
+        map.locate()
+      },
+      locationfound(e) {
+        setPosition(e.latlng)
+        map.flyTo(e.latlng, map.getZoom())
+      },
+    })
+  
+    console.log(position);
+    return position === null ? null : (
+      <Marker position={position}>
+        <Popup>You are here</Popup>
+      </Marker>
+    )
+  }
+
+export default LocationMarker;
